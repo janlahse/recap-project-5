4aa3d862b64edd52aeb8fe894bd3cfa683f84947
Gregor Wedlich <hello@sil3ntrunning.net>
2025-05-09 16:10:15 +0200

    Refactor: Make Pieces detail page dynamic, some refactors

--- pages/details/index.js
+++
@@ -1,5 +0,0 @@
-import { DetailsPage } from "@/components/DetailsPage/DetailsPage";
-
-export default function DetailsListPage({ pieces, isLoading }) {
-  return <DetailsPage pieces={pieces} isLoading={isLoading} />;
-}
