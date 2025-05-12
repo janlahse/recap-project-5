db8f0da8f08b7656f8655f4ac39145487505ca6c
Gregor Wedlich <hello@sil3ntrunning.net>
2025-05-12 13:13:30 +0200

    refactor: simplify piece retrieval logic in PiecesDetailPage

--- pages/gallery-pieces/[slug]/index.js
+++ pages/gallery-pieces/[slug]/index.js
@@ -1,16 +1,11 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import { DetailsPage } from "@/components/DetailsPage/DetailsPage";
 
 export default function PiecesDetailPage({ pieces, isLoading }) {
-  const [piece, setPiece] = useState(null);
   const router = useRouter();
-
   const { slug } = router.query;
 
-  useEffect(() => {
-    setPiece(pieces?.find((piece) => piece.slug === slug));
-  }, [piece, pieces, slug]);
+  const piece = pieces?.find((p) => p.slug === slug);
 
   if (!piece) {
     return null;
